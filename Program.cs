e0345ce22e96f99c58de7f5b158a1a8e30431e33
Viktor <viktor.ivanov@extrawest.com>
2023-05-09 20:48:54 +0300

    lr3

--- Program.cs
+++ Program.cs
@@ -1,91 +1,126 @@
-﻿class Program
+﻿static void ProcessData(object data)
 {
-    private static void LoremSplit()
-    {
-
-        Console.WriteLine("Enter the amount of words: ");
-        int wordsAmount = int.Parse(Console.ReadLine());
-
-        string text = File.ReadAllText("./lorem.txt");
-
-        string[] words = text.Split(' ');
-        for (int i = 0; i < wordsAmount; i++)
-        {
-            Console.Write(words[i] + " ");
-        }
-        Console.WriteLine('\n');
-    }
-    // Method to display Mathematic Operations
-
-    private static void MathOperation()
-    {
-        bool exit = false;
-        while (!exit)
-        {
-            Console.WriteLine("1) +");
-            Console.WriteLine("2) -");
-            Console.WriteLine("3) *");
-            Console.WriteLine("4) /");
-            Console.WriteLine("5) Exit");
-            int operation = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("First number:");
-            double a = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Second number:");
-            double b = double.Parse(Console.ReadLine());
-
-            double result = 0;
-            switch (operation)
-            {
-                case 1:
-                    result = a + b;
-                    break;
-                case 2:
-                    result = a - b;
-                    break;
-                case 3:
-                    result = a * b;
-                    break;
-                case 4:
-                    result = a / b;
-                    break;
-                case 5:
-                    exit = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid operation.");
-                    break;
-            }
-            Console.WriteLine("Res: " + result);
-        }
-    }
-
-    static void Main(string[] args)
-    {
-        bool exit = false;
-        while (!exit)
-        {
-            Console.WriteLine("1) Display number of words from text");
-            Console.WriteLine("2) Perform mathematical operation");
-            Console.WriteLine("3) Exit");
-
-            int selection = int.Parse(Console.ReadLine());
-            switch (selection)
-            {
-                case 1:
-                    LoremSplit();
-                    break;
-                case 2:
-                    MathOperation();
-                    break;
-                case 3:
-                    exit = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid selection.");
-                    break;
-            }
-        }
-    }
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} started.");
+    // Імітація обробки великих об'ємів даних
+    Thread.Sleep(5000);
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished.");
 }
+
+static void ParallelProcessing()
+{
+    Console.WriteLine("Processing started.");
+
+    int[] data = new int[1000];
+    Random rand = new Random();
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] = rand.Next(1, 10);
+    }
+
+    int chunkSize = 100;
+    for (int i = 0; i < data.Length; i += chunkSize)
+    {
+        int[] chunk = new int[chunkSize];
+        Array.Copy(data, i, chunk, 0, chunkSize);
+        Thread thread = new Thread(ProcessData);
+        thread.Start(chunk);
+    }
+
+    Console.WriteLine("Processing finished.");
+}
+
+Mutex mutex = new Mutex();
+
+void UseMutexFunc(string message)
+{
+    mutex.WaitOne();
+    Console.WriteLine($"Mutex Thread {Thread.CurrentThread.ManagedThreadId} started.");
+    Thread.Sleep(2000);
+    Console.WriteLine($"Mutex Thread {Thread.CurrentThread.ManagedThreadId} finished. {message}");
+    mutex.ReleaseMutex();
+}
+
+void SynchronizedAccess()
+{
+    Console.WriteLine("Synchronized access started.");
+
+    // Симуляція декількох потоків, які одночасно працюють з одним mutex об'єктом
+    Thread[] threads = new Thread[5];
+    for (int i = 0; i < threads.Length; i++)
+    {
+        threads[i] = new Thread(() => UseMutexFunc($"Message {i}"));
+        threads[i].Start();
+    }
+
+    Console.WriteLine("Synchronized access finished.");
+}
+
+
+void Method1()
+{
+    Console.WriteLine("Thread 1 started");
+    Thread.Sleep(2000);
+    Console.WriteLine("Thread 1 ended");
+}
+
+void Method2(object obj)
+{
+    int number = (int)obj;
+    Console.WriteLine($"Thread 2 started with number {number}");
+    Thread.Sleep(3000);
+    Console.WriteLine($"Thread 2 ended with number {number}");
+}
+
+void Method3()
+{
+    Thread.CurrentThread.Name = "MyThread";
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} started");
+    Thread.Sleep(4000);
+    Console.WriteLine($"Thread {Thread.CurrentThread.Name} ended");
+}
+
+
+async Task Method1Async()
+{
+    Console.WriteLine("Method 1 started");
+    await Task.Delay(2000);
+    Console.WriteLine("Method 1 ended");
+}
+
+async Task Method2Async()
+{
+    Console.WriteLine("Method 2 started");
+    Task task1 = Task.Delay(3000);
+    Task task2 = Task.Delay(2000);
+    await Task.WhenAll(task1, task2);
+    Console.WriteLine("Method 2 ended");
+}
+
+async Task<int> Method3Async()
+{
+    Console.WriteLine("Method 3 started");
+    await Task.Delay(4000);
+    Console.WriteLine("Method 3 ended");
+    return 42;
+}
+
+void Main()
+{
+    Thread thread1 = new Thread(Method1);
+    Thread thread2 = new Thread(new ParameterizedThreadStart(Method2));
+    Thread thread3 = new Thread(Method3);
+
+    thread1.Start();
+    thread2.Start(42);
+    thread3.Start();
+
+    SynchronizedAccess();
+    ParallelProcessing();
+
+    Method1Async().Wait();
+    Method2Async().Wait();
+    int result = Method3Async().Result;
+    Console.WriteLine($"Method 3 returned {result}");
+}
+
+Main();
